9287f6e425afb4b97be60009606228884bc7be97
The Doctor <drwho@virtadpt.net>
2011-06-05 22:00:25 -0400

    - Shaking the bugs out of the script.  Also working out how the graphcs are   generated.  I hate RRDtool.

--- scripts/traffic_stats.sh
+++ scripts/traffic_stats.sh
@@ -40,7 +40,6 @@ if [ ! -d "$GRAPHS" ]; then
 
 # Set up the loop that updates everything once a minute.
 while true; do
-
 	# Pick out all of the network interfaces on the box.
 	for i in `ifconfig | grep '^[a-z]' | grep -v '^lo' | awk '{print $1}'`;do
 		INTERFACES="$INTERFACES $i"
@@ -53,43 +52,49 @@ while true; do
 			rrdtool create "$DATABASES/$i.rrd" -s 300 \
 	                DS:in:DERIVE:600:0:12500000 \
 			DS:out:DERIVE:600:0:12500000 \
-        	        RRA:AVERAGE:0.5:1:576 RRA:AVERAGE:0.5:6:672 \
-                	RRA:AVERAGE:0.5:24:732 RRA:AVERAGE:0.5:144:1460
+        	        RRA:AVERAGE:0.5:1:576 \
+			RRA:AVERAGE:0.5:6:672 \
+                	RRA:AVERAGE:0.5:24:732 \
+			RRA:AVERAGE:0.5:144:1460
 			fi
 		done
 
 	# Extract the bytes sent and received by each network interface.
 	for i in $INTERFACES; do
 		TRAFFIC=`ifconfig "$i" | grep bytes | awk '{print $2, $6}' | sed 's/bytes://g'`
-		IN=`echo $TRAFFIC | awk '{print $3}'`
-		OUT=`echo $TRAFFIC | awk '{print $4}'`
+		IN=`echo $TRAFFIC | awk '{print $1}'`
+		OUT=`echo $TRAFFIC | awk '{print $2}'`
 
 		# Update the appropriate RRDtool database with the IN and OUT
 		# stats.
-		rrdtool update "$DATABASES/$i.rrd" -t in:out N:"$IN":"$OUT"
+		rrdtool update "$DATABASES/$i.rrd" -t "in:out" "N:$IN:$OUT"
 		done
 
 	# Update the graphs for the status page.
 	for i in $INTERFACES; do
-		for j in "daily weekly monthly yearly"; do
-			rrdtool graph "$GRAPHS/$i-$j.png" \
-			-t "$j traffic on $i." \
-			--lazy -h 80 -w 600 -l 0 -a PNG -v "bytes/sec" \
-			DEF:in="$DATABASES/$i.rrd":in:AVERAGE \
-			DEF:out="$DATABASES/$i.rrd":out:AVERAGE \
-			CDEF:"out_neg=out,-1,*" \
-			AREA:in#32CD32:Incoming \
-			LINE1:in#336600 \
-			GPRINT:in:MAX:"  Max\\: %5.1lf %s" \
-			GPRINT:in:AVERAGE:" Avg\\: %5.1lf %S" \
-			GPRINT:in:LAST:" Current\\: %5.1lf %Sbytes/sec\\n" \
-			AREA:out_neg#4169E1:Outgoing \
-			LINE1:out_neg#0033CC \
-			GPRINT:out:MAX:"  Max\\: %5.1lf %S" \
-			GPRINT:out:AVERAGE:" Avg\\: %5.1lf %S" \
-			GPRINT:out:LAST:" Current\\: %5.1lf %Sbytes/sec" \
-			HRULE:0#000000 1>/dev/null
-			done
+		# Generate time_t values for the start and end of the graphs.
+		START=`date -d "1 hour ago" +%s`
+		END=`date +%s`
+
+		# Build the graph
+		rrdtool graph "$GRAPHS/$i.png" \
+		-s $START -e $END \
+		-t "Traffic on $i." \
+		--lazy -h 80 -w 600 -l 0 -a PNG -v "bytes/minute" \
+		DEF:in=$DATABASES/$i.rrd:in:AVERAGE \
+		DEF:out=$DATABASES/$i.rrd:out:AVERAGE \
+		CDEF:"out_neg=out,-1,*" \
+		AREA:in#32CD32:Incoming \
+		LINE1:in#336600 \
+		GPRINT:in:MAX:"  Max\\: %5.1lf %s" \
+		GPRINT:in:AVERAGE:" Avg\\: %5.1lf %S" \
+		GPRINT:in:LAST:" Current\\: %5.1lf %Sbytes/sec\\n" \
+		AREA:out_neg#4169E1:Outgoing \
+		LINE1:out_neg#0033CC \
+		GPRINT:out:MAX:"  Max\\: %5.1lf %S" \
+		GPRINT:out:AVERAGE:" Avg\\: %5.1lf %S" \
+		GPRINT:out:LAST:" Current\\: %5.1lf %Sbytes/sec" \
+		HRULE:0#000000 1>/dev/null
 		done
 
 	# Bottom of the loop.
