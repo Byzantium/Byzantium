062316234612c1b15f17993e404d599de5ea380e
Ben Mendis <dragonwisard@gmail.com>
2011-06-25 13:53:30 -0400

    Starting on a connection script for babel

---
+++ scripts/babel.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BABELD=${BABELD:-"babeld"}
+AHCPD=${AHCPD:-"ahcpd"}
+
+WDEV=${1:-"wlan0"}
+ESSID=${2:-"hacdc-babel"}
+CHAN=${3:-"9"}
+
+connect_to_adhoc() {
+	/sbin/ifconfig ${WDEV} down
+	/sbin/iwconfig ${WDEV} mode ad-hoc essid ${ESSID} channel ${CHAN}
+	/sbin/ifconfig ${WDEV} up
+}
+
+start_babel() {
+	$BABELD -D -g 33123 ${WDEV}
+}
+
+start_ahcpd() {
+	$AHCPD -D ${WDEV}
+}
+
+main() {
+	connect_to_adhoc
+	start_babel
+	start_ahcpd
+}
+
+main
+
