a1ac92982d9d45bdedc3adbfe4b74f55a3cbc862
Ben Mendis <bmendis@tapdrive.com>
2011-06-25 15:02:00 -0400

    Adding kill_nm() to kill Network Manager
    Also flushes the firewall.

--- scripts/babel.sh
+++ scripts/babel.sh
@@ -7,6 +7,16 @@ WDEV=${1:-"wlan0"}
 ESSID=${2:-"hacdc-babel"}
 CHAN=${3:-"9"}
 
+kill_nm() {
+	#Fedora
+	/etc/init.d/NetworkManager stop >/dev/null 2>&1 || true
+
+	#Ubuntu
+	stop network-manager >/dev/null 2>&1 || true
+
+	/sbin/iptables -F
+}
+
 connect_to_adhoc() {
 	/sbin/ifconfig ${WDEV} down
 	/sbin/iwconfig ${WDEV} mode ad-hoc essid ${ESSID} channel ${CHAN}
@@ -22,6 +32,7 @@ start_ahcpd() {
 }
 
 main() {
+	kill_nm
 	connect_to_adhoc
 	start_babel
 	start_ahcpd
