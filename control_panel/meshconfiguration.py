0c2029bd44d011337f54d1a0fcda5fac969b9bc2
The Doctor <drwho@virtadpt.net>
2011-08-06 22:16:12 -0400

    - Debugged the MeshConfiguration.addtomesh() method.  Got the .exposed call   wrong. - Added MeshConfiguration.enable() to run babeld.  If babeld isn't running it   starts it up with the wireless interface configured by the user.  If the user   is adding an interface to a group of them on the same machine, it will detect   this condition and restart babeld appropriately. - Tweaked the HTML templates a little.

--- control_panel/meshconfiguration.py
+++ control_panel/meshconfiguration.py
@@ -7,12 +7,14 @@
 # rewritten to support a different (or more) protocols.
 
 # TODO:
+# - Test this more!
 # - Make network interfaces that don't exist anymore go away.
 # - Detect when an interface is already routing and instead offer the ability
 #   to remove it from the mesh.  Do that in the MeshConfiguration.index()
 #   method.
 # - Switch out the development configuration databases for the production
 #   databases.
+# - Refactor code to split PID getting into a helper method.
 
 # Import external modules.
 import cherrypy
@@ -20,6 +22,9 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 from mako.exceptions import RichTraceback
 import sqlite3
+import os
+import time
+import subprocess
 
 # Import core control panel modules.
 from control_panel import *
@@ -29,7 +34,9 @@ from control_panel import *
 # interfaces.
 class MeshConfiguration(object):
     # Class constants.
-    babeld = '/usr/sbin/babeld'
+    babeld = '/usr/bin/babeld'
+    babeld_pid = '/var/run/babeld.pid'
+    babeld_timeout = 3
 
     #netconfdb = '/var/db/network.sqlite'
     netconfdb = '/home/drwho/network.sqlite'
@@ -43,6 +50,7 @@ class MeshConfiguration(object):
     interface = ''
     protocol = ''
     enabled = ''
+    pid = ''
 
     # Pretends to be index.html.
     def index(self):
@@ -120,6 +128,7 @@ class MeshConfiguration(object):
         self.interface = ''
         self.protocol = ''
         self.enabled = ''
+        self.pid = ''
 
     # Allows the user to add a wireless interface to the mesh.  Assumes that
     # the interface is already configured (we wouldn't get this far if it
@@ -148,5 +157,95 @@ class MeshConfiguration(object):
                 print "Execution died on line %s\n" % line
                 print "%s: %s" % (str(traceback.error.__class__.__name__),
                     traceback.error)
-    index.exposed = True
+    addtomesh.exposed = True
 
+    # Runs babeld to turn self.interface into a mesh interface.
+    def enable(self):
+        # Set up the error and successful output messages.
+        error = ''
+        output = ''
+
+        # Set up a default set of command line options for babeld.  Some of
+        # these are redundant but are present in case an older version of
+        # babeld is used on the node.  See entries in the following file for
+        # 30 January 2011 (release of babeld v1.1.0) to understand why.
+        # http://www.pps.jussieu.fr/~jch/software/babel/CHANGES.text
+        common_babeld_opts = ['-m ff02:0:0:0:0:0:1:6', '-p 6697', '-D']
+
+        # Create a set of unique command line options for babeld.  Right now,
+        # this variable is empty but it might be used in the future.  Maybe
+        # it'll be populated from a config file or something.
+        unique_babeld_opts = []
+
+        # Set up a list of mesh interfaces for which babeld is already running.
+        interfaces = []
+        connection = sqlite3.connect(self.meshconfdb)
+        cursor = connection.cursor()
+        cursor.execute("SELECT interface, enabled, protocol FROM meshes WHERE enabled='yes' AND protocol='babel';")
+        results = cursor.fetchall()
+        for i in results:
+            interfaces.append(i[0][0])
+
+        # By definition, if we're in this method the new interface hasn't been
+        # added yet.
+        interfaces.append(self.interface)
+
+        # Assemble the invocation of babeld.
+        babeld_command = []
+        babeld_command.append(self.babeld)
+        babeld_command = babeld_command + common_babeld_opts
+        babeld_command = babeld_command + unique_babeld_opts + interfaces
+
+        # Test to see if babeld is running.  If it is, it's routing for at
+        # least one interface, in which case we add the one the user just
+        # picked to the list because we'll have to restart babeld.  Otherwise,
+        # we just start babeld.
+        pid = ''
+        if os.path.exists(self.babeld_pid):
+            pidfile = open(self.babeld_pid, 'r')
+            pid = pidfile.readline()
+            pidfile.close()
+        if pid:
+            os.kill(int(pid), signal.SIGTERM)
+            time.sleep(self.babeld_timeout)
+        process = subprocess.Popen(babeld_command)
+        time.sleep(self.babeld_timeout)
+
+        # Get the PID of babeld, then test to see if that pid exists and
+        # corresponds to a running babeld process.  If there is no match,
+        # babeld isn't running.
+        pid = ''
+        if os.path.exists(self.babeld_pid):
+            pidfile = open(self.babeld_pid, 'r')
+            pid = pidfile.readline()
+            pidfile.close()
+        if pid:
+            procdir = '/proc/' + pid
+            if not os.path.isdir(procdir):
+                error = "ERROR: babeld is not running!  Did it crash after startup?"
+            else:
+                output = babeld + " has been successfully started with PID " + pid + "."
+                # Update the mesh configuration database to take into account
+                # the presence of the new interface.
+                template = ('yes', self.interface, )
+                cursor.execute("UPDATE meshes SET enabled=? WHERE interface=?;", template)
+                cursor.commit()
+        cursor.close()
+
+        # Render the HTML page.
+        try:
+            page = templatelookup.get_template("/mesh/enabled.html")
+            return page.render(title = "Byzantium Node Mesh Configuration",
+                               purpose_of_page = "Mesh Interface Enabled",
+                               protocol = self.protocol,
+                               interface = self.interface,
+                               error = error, output = output)
+        except:
+            traceback = RichTraceback()
+            for (filename, lineno, function, line) in traceback.traceback:
+                print "\n"
+                print "Error in file %s\n\tline %s\n\tfunction %s" % (filename, lineno, function)
+                print "Execution died on line %s\n" % line
+                print "%s: %s" % (str(traceback.error.__class__.__name__),
+                    traceback.error)
+    enable.exposed = True
