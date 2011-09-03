7abfd819905f5e7b40dd982ae0aad82e72fec161
The Doctor <drwho@virtadpt.net>
2011-09-03 23:00:45 +0000

    - Checking the slackbuild scripts for nginx v1.0.5 into Git beecause I've had to modify   a lot of them to be Porteus-specific and Byzantium-specific. - Initial check-in of etc/rc.d/rc.ssl, which tests to see if SSL certs exist for nginx   and create them if they dont.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ porteus/nginx/doinst.sh
@@ -0,0 +1,27 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname ${NEW})/$(basename ${NEW} .new)"
+  if [ -e ${OLD} ]; then
+    cp -a ${OLD} ${NEW}.incoming
+    cat ${NEW} > ${NEW}.incoming
+    mv ${NEW}.incoming ${NEW}
+  fi
+  config ${NEW}
+}
+
+preserve_perms etc/rc.d/rc.nginx.new
+preserve_perms etc/rc.d/rc.ssl.new
+config etc/logrotate.d/nginx.new
