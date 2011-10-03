2c0f73ab668ef399f0a98ee06927331a6591112a
root <root@pinhead.localdomain>
2011-10-03 01:27:04 -0400

    add ahcpd module modify build.sh to actually export the $1.info values for the slackbuilds massage git some more (still not working for me) add node module

--- porteus/build.sh
+++ porteus/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+set -x
 set -e
 
 cd $1
 source ./$1.info
+for i in `cat ./$1.info | cut -d = -f 1` ; do
+    export $i
+done
+
 wget --no-check-certificate -nc $DOWNLOAD
 cd ../../pkgs
 export OUTPUT=`pwd`
