74446a3876c8cc9b8a92733bb034167dbdb0d684
The Doctor <drwho@virtadpt.net>
2011-10-13 20:44:46 -0400

    - Added a text field 'gateway' to the network.sqlite database. - Updated the .sql script to take the new field into account.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- control_panel/var/db/controlpanel/network.sql
+++ control_panel/var/db/controlpanel/network.sql
@@ -1,4 +1,4 @@
 BEGIN TRANSACTION;
 CREATE TABLE wired (enabled TEXT, gateway TEXT, interface TEXT);
-CREATE TABLE wireless (client_interface TEXT, enabled TEXT, channel NUMERIC, essid TEXT, mesh_interface TEXT);
+CREATE TABLE wireless (gateway TEXT, client_interface TEXT, enabled TEXT, channel NUMERIC, essid TEXT, mesh_interface TEXT);
 COMMIT;
