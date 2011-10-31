67a97f240b15933e25463b08fded51a9fbea875c
The Doctor <drwho@virtadpt.net>
2011-10-30 22:31:13 -0400

    - Added two new HTML templates for the mesh configuration system, disabled.html   and removefrommesh.html.  Those are used for pretty much what you would   expect. - Updated mesh/index.html to have a second table, this one for active mesh   interfaces that can be taken down by the user. - Added methods to meshconfiguration.py that allow the user to select a mesh   interface and take it out of the mesh (while leaving it configured, just not   running).
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- control_panel/meshconfiguration.py
+++ control_panel/meshconfiguration.py
@@ -10,13 +10,10 @@
 # License: GPLv3
 
 # TODO:
-# - Test this more!
 # - Make network interfaces that don't exist anymore go away.
 # - Detect when an interface is already routing and instead offer the ability
 #   to remove it from the mesh.  Do that in the MeshConfiguration.index()
 #   method.
-# - Switch out the development configuration databases for the production
-#   databases.
 # - Refactor code to split PID getting into a helper method.
 
 # Import external modules.
@@ -34,19 +31,18 @@ import signal
 from control_panel import *
 
 # Classes.
-# Allows the user to configure to configure mesh networking on wireless network
-# interfaces.
+# Allows the user to configure mesh networking on wireless network interfaces.
 class MeshConfiguration(object):
     # Class constants.
     babeld = '/usr/local/bin/babeld'
     babeld_pid = '/var/run/babeld.pid'
     babeld_timeout = 3
 
-    netconfdb = '/var/db/controlpanel/network.sqlite'
-    #netconfdb = '/home/drwho/network.sqlite'
+    #netconfdb = '/var/db/controlpanel/network.sqlite'
+    netconfdb = '/home/drwho/network.sqlite'
 
-    meshconfdb = '/var/db/controlpanel/mesh.sqlite'
-    #meshconfdb = '/home/drwho/mesh.sqlite'
+    #meshconfdb = '/var/db/controlpanel/mesh.sqlite'
+    meshconfdb = '/home/drwho/mesh.sqlite'
 
     # Class attributes which apply to a network interface.  By default they
     # are blank but will be populated from the mesh.sqlite database if the
@@ -82,6 +78,7 @@ class MeshConfiguration(object):
 
             # Walk through the list of results.
             interfaces = ''
+            active_interfaces = ''
             for i in results:
                 if i[1] == 'yes':
                     # See if the interface is already in the mesh configuration
@@ -103,8 +100,8 @@ class MeshConfiguration(object):
                     # so add it to the template as an unclickable button.
                     # While it might not be a good idea to put unusable buttons
                     # into the page, it would tell the user that the interfaces
-                    # were detected and are usable.
-                    interfaces = interfaces + "<input type='submit' name='junk' value='" + i[0] + "' style='background-color:grey;' />\n"
+                    # were detected.
+                    active_interfaces = active_interfaces + "<input type='submit' name='interface' value='" + i[0] + "' style='background-color:grey;' />\n"
             meshconfcursor.close()
 
         # Clean up our connections to the configuration databases.
@@ -115,7 +112,8 @@ class MeshConfiguration(object):
             page = templatelookup.get_template("/mesh/index.html")
             return page.render(title = "Byzantium Node Mesh Configuration",
                                purpose_of_page = "Configure Mesh Interfaces",
-                               error = error, interfaces = interfaces)
+                               error = error, interfaces = interfaces,
+                               active_interfaces = active_interfaces)
         except:
             traceback = RichTraceback()
             for (filename, lineno, function, line) in traceback.traceback:
@@ -252,3 +250,128 @@ class MeshConfiguration(object):
                 print "%s: %s" % (str(traceback.error.__class__.__name__),
                     traceback.error)
     enable.exposed = True
+
+    # Allows the user to remove a configured interface from the mesh.  Takes
+    # one argument from self.index(), the name of the interface.
+    def removefrommesh(self, interface=None):
+        # Configure this instance of the object for the interface the user
+        # wants to remove from the mesh.
+        self.interface = interface
+        self.protocol = 'babel'
+        self.enabled = 'yes'
+
+        # Render the HTML page.
+        try:
+            page = templatelookup.get_template("/mesh/removefrommesh.html")
+            return page.render(title = "Byzantium Node Mesh Configuration",
+                               purpose_of_page = "Disable Mesh Interface",
+                               interface = interface)
+        except:
+            traceback = RichTraceback()
+            for (filename, lineno, function, line) in traceback.traceback:
+                print "\n"
+                print "Error in file %s\n\tline %s\n\tfunction %s" % (filename, lineno, function)
+                print "Execution died on line %s\n" % line
+                print "%s: %s" % (str(traceback.error.__class__.__name__),
+                    traceback.error)
+    removefrommesh.exposed = True
+
+    # Re-runs babeld without self.interface to drop it out of the mesh.
+    def disable(self):
+        # Set up the error and successful output messages.
+        error = ''
+        output = ''
+
+        # Set up a default set of command line options for babeld.  Some of
+        # these are redundant but are present in case an older version of
+        # babeld is used on the node.  See the following file to see why:
+        # http://www.pps.jussieu.fr/~jch/software/babel/CHANGES.text
+        common_babeld_opts = ['-m', 'ff02:0:0:0:0:0:1:6', '-p', '6696', '-D']
+
+        # Create a set of unique command line options for babeld.  Right now,
+        # this variable is empty but it might be used in the future.  Maybe
+        # it'll be populated from a config file or something.
+        unique_babeld_opts = []
+
+        # Set up a list of mesh interfaces for which babeld is already running
+        # but omit self.interface.
+        interfaces = []
+        connection = sqlite3.connect(self.meshconfdb)
+        cursor = connection.cursor()
+        cursor.execute("SELECT interface FROM meshes WHERE enabled='yes' AND protocol='babel';")
+        results = cursor.fetchall()
+        for i in results:
+            if i[0] != self.interface:
+                interfaces.append(i)
+
+        # If there are no mesh interfaces configured anymore, then the node
+        # is offline.
+        if not len(interfaces):
+            output = 'Byzantium node offline.'
+
+        # Assemble the invocation of babeld.
+        babeld_command = []
+        babeld_command.append(self.babeld)
+        babeld_command = babeld_command + common_babeld_opts
+        babeld_command = babeld_command + unique_babeld_opts + interfaces
+
+        # Test to see if babeld is running.  If it is, we restart it without
+        # the network interface that the user wants to drop out of the mesh.
+        pid = ''
+        if os.path.exists(self.babeld_pid):
+            pidfile = open(self.babeld_pid, 'r')
+            pid = pidfile.readline()
+            pidfile.close()
+        if pid:
+            os.kill(int(pid), signal.SIGTERM)
+            time.sleep(self.babeld_timeout)
+
+        # If there is at least one wireless network interface still configured,
+        # then re-run babeld.
+        if len(interfaces):
+            process = subprocess.Popen(babeld_command)
+            time.sleep(self.babeld_timeout)
+
+        # Get the PID of babeld, then test to see if that pid exists and
+        # corresponds to a running babeld process.  If there is no match,
+        # babeld isn't running, in which case something went wrong.
+        pid = ''
+        if os.path.exists(self.babeld_pid):
+            pidfile = open(self.babeld_pid, 'r')
+            pid = pidfile.readline()
+            pidfile.close()
+        if pid:
+            procdir = '/proc/' + pid
+            if not os.path.isdir(procdir):
+                error = "ERROR: babeld is not running!  Did it crash during startup?"
+            else:
+                output = self.babeld + " has been successfully started with PID " + pid + "."
+                # Update the mesh configuration database to take into account
+                # the presence of the new interface.
+                template = ('yes', self.interface, )
+                cursor.execute("UPDATE meshes SET enabled=? WHERE interface=?;", template)
+        else:
+            # There are no mesh interfaces left, so update the database to
+            # deconfigure self.interface.
+            template = ('no', self.interface, )
+            cursor.execute("UPDATE meshes SET enabled=? WHERE interface=?;",
+                           template)
+        connection.commit()
+        cursor.close()
+
+        # Render the HTML page.
+        try:
+            page = templatelookup.get_template("/mesh/disabled.html")
+            return page.render(title = "Byzantium Node Mesh Configuration",
+                               purpose_of_page = "Disable Mesh Interface",
+                               error = error, output = output)
+        except:
+            traceback = RichTraceback()
+            for (filename, lineno, function, line) in traceback.traceback:
+                print "\n"
+                print "Error in file %s\n\tline %s\n\tfunction %s" % (filename, lineno, function)
+                print "Execution died on line %s\n" % line
+                print "%s: %s" % (str(traceback.error.__class__.__name__),
+                    traceback.error)
+    removefrommesh.exposed = True
+    disable.exposed = True
