3a401085a637f823ab72455223485e3c3d814587
Ben Mendis <dragonwisard@gmail.com>
2012-03-31 18:51:40 -0400

    Adding a script to automate much of the build process
    This is part of the soultion for issue #70
    
    Squashed commit of the following:
    
    commit 1c73dcaf2c0c54baee40f2f034584c6ef796e46f
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:48:51 2012 -0400
    
        Renaming this script to something more appropriate
    
    commit c74e06f060e778cc0a848c92d3c04ee91f160c83
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:45:53 2012 -0400
    
        forgot a cruicial last step
    
    commit b3afe457dcf034f84fb9220a9781ca2fca127096
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:34:02 2012 -0400
    
        Hmm... this file doesn't exit
    
    commit 7be6e51a5e362f6cd21e32aadcda85d0135573f1
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:29:01 2012 -0400
    
        typo
    
    commit 95ab4133d3411cca8265e34ffd36e1e17002aa9a
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:27:17 2012 -0400
    
        typo
    
    commit 70653649c85a1bbf0cf3b0c030221f64bfbbd438
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:24:43 2012 -0400
    
        typo
    
    commit 1465f57ed82fbb22ba897b7fae487a3fa5b322cf
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:21:05 2012 -0400
    
        typo
    
    commit a2c9907eef29f968ce793882ac3fb3d8d2cc47c4
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:15:12 2012 -0400
    
        typo
    
    commit 5f1041fde3752ba670be55bb9d13231a4a07a847
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:13:37 2012 -0400
    
        typo
    
    commit ac15d2646a4c4383aea75552c3dfed2b886e6f96
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:10:37 2012 -0400
    
        typo
    
    commit 9efc0a83e937f151a23cf92a53dcc4f22e0e06fb
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:07:01 2012 -0400
    
        Bail out if there are any errors
    
    commit 08720224c5bdab47106f4398b7fc06c52c74b89c
    Author: Ben Mendis <dragonwisard@gmail.com>
    Date:   Sat Mar 31 18:01:20 2012 -0400
    
        Trying to automate the build process outlined on the wiki
    
        http://wiki.hacdc.org/index.php/Process_for_manually_installing_Byzantium.

---
+++ build-final-module.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+# Bail on errors
+set -e
+
+cd ~guest/byzantium
+mkdir -p /tmp/fakeroot
+for i in *.xzm ; do xzm2dir $i /tmp/fakeroot ; done
+
+# Should these steps be necessary? Maybe it should be fixed in the module.
+rm /tmp/fakeroot/srv/httpd
+rm /tmp/fakeroot/srv/www
+mkdir -p /tmp/fakeroot/srv/httpd/databases
+cd /tmp/fakeroot/srv
+ln -s httpd www
+
+# We should build a controlpanel module to obviate these steps.
+mkdir -p /tmp/fakeroot/srv/controlpanel/graphs
+cp -rv ~guest/Byzantium/control_panel/srv/controlpanel/* /tmp/fakeroot/srv/controlpanel
+mkdir -p /tmp/fakeroot/etc/controlpanel
+cp ~guest/Byzantium/control_panel/etc/controlpanel/* /tmp/fakeroot/etc/controlpanel
+mkdir -p /tmp/fakeroot/var/db/controlpanel
+cp -rv ~guest/Byzantium/control_panel/var/db/controlpanel/* /tmp/fakeroot/var/db/controlpanel
+
+# We need to upgrade the version of Wicd to fix a bug, this is a hackaround.
+mkdir -p /tmp/fakeroot/usr/share/wicd/cli
+cp ~guest/Byzantium/porteus/wicd/usr/share/wicd/cli/wicd-cli.py /tmp/fakeroot/usr/share/wicd/cli/
+
+
+cd ~guest/Byzantium/scripts
+mkdir -p /tmp/fakeroot/etc/udev/rules.d
+cp 11-media-by-label-auto-mount.rules /tmp/fakeroot/etc/udev/rules.d
+
+# Could these be placed in a module?
+cp rc.local rc.mysqld rc.setup_mysql /tmp/fakeroot/etc/rc.d
+
+# Do we really want to enable _everything_?
+chmod +x /tmp/fakeroot/etc/rc.d/rc.*
+
+# This stuff probably belongs in the controlpanel package
+cp traffic_stats.sh /tmp/fakeroot/usr/local/bin
+cd ../control_panel
+mkdir -p /tmp/fakeroot/usr/local/sbin
+cp *.py /tmp/fakeroot/usr/local/sbin
+cp etc/rc.d/rc.byzantium /tmp/fakeroot/etc/rc.d/
+mkdir -p /tmp/fakeroot/etc/ssl
+cp etc/ssl/openssl.cnf /tmp/fakeroot/etc/ssl
+
+cd ../porteus
+mkdir -p /tmp/fakeroot/home/guest/.mozilla/firefox/c3pp43bg.default
+cp home/guest/.mozilla/firefox/c3pp43bg.default/prefs.js /tmp/fakeroot/home/guest/.mozilla/firefox/c3pp43bg.default
+
+# Why aren't these in their modules?
+cp -rv apache/etc/httpd/* /tmp/fakeroot/etc/httpd
+cp babel/babeld.conf /tmp/fakeroot/etc
+cp dnsmasq/dnsmasq.conf /tmp/fakeroot/etc
+cp etherpad-lite/rc.etherpad-lite /tmp/fakeroot/etc/rc.d
+
+cp -rv ifplugd/etc/ifplugd/* /tmp/fakeroot/etc/ifplugd
+
+cp etc/passwd /tmp/fakeroot/etc
+cp etc/shadow /tmp/fakeroot/etc
+cp etc/hosts /tmp/fakeroot/etc
+cp etc/HOSTNAME /tmp/fakeroot/etc
+cp etc/inittab /tmp/fakeroot/etc
+chown root:root /tmp/fakeroot/etc/passwd /tmp/fakeroot/etc/shadow
+chmod 0600 /tmp/fakeroot/etc/shadow
+
+# These belong in modules!
+cp mysql/my.cnf /tmp/fakeroot/etc
+cp ngircd/ngircd.conf /tmp/fakeroot/etc
+cp ngircd/rc.ngircd /tmp/fakeroot/etc/rc.d
+cp php/etc/httpd/php.ini /tmp/fakeroot/etc/httpd
+
+# This should be a module
+mkdir -p /tmp/fakeroot/opt/qwebirc
+cp qwebirc/config.py /tmp/fakeroot/opt/qwebirc
+cp qwebirc/rc.qwebirc /tmp/fakeroot/etc/rc.d
+
+cd ..
+cp databases/* /tmp/fakeroot/srv/httpd/databases
+
+mkdir -p /tmp/fakeroot/home/guest/Desktop
+cp porteus/home/guest/Desktop/Control\ Panel.desktop /tmp/fakeroot/home/guest/Desktop
+mkdir -p /tmp/fakeroot/usr/share/pixmaps/porteus
+cp byzantium-icon.png /tmp/fakeroot/usr/share/pixmaps/porteus
+
+# has fail??
+mkdir -p /tmp/fakeroot/srv/httpd/htdocs
+cp -rv /srv/httpd/htdocs/* /tmp/fakeroot/srv/httpd/htdocs/
+
+mkdir /tmp/fakeroot/var/run/ngircd
+chown ngircd.root /tmp/fakeroot/var/run/ngircd
+chmod 0750 /tmp/fakeroot/var/run/ngircd
+
+mkdir -p /tmp/fakeroot/srv/captiveportal
+mkdir -p /tmp/fakeroot/etc/captiveportal
+cd ~guest/Byzantium/captive_portal
+cp captive_portal.py /tmp/fakeroot/usr/local/sbin
+cp captive-portal.sh /tmp/fakeroot/usr/local/sbin
+cp etc/captiveportal/captiveportal.conf /tmp/fakeroot/etc/captiveportal/
+cp srv/captiveportal/* /tmp/fakeroot/srv/captiveportal/
+
+chown -R guest:guest /tmp/fakeroot/home/guest
+
+# This file doesn't exit?
+#chmod -x /tmp/fakeroot/etc/rc.d/rc3.d/S-firewall
+
+dir2xzm /tmp/fakeroot /tmp/000-byzantium.xzm
+
