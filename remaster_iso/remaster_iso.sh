cb43ea989b5495d725361d3b0ef45cccaefad8ff
The Doctor <drwho@virtadpt.net>
2012-05-26 17:41:58 -0400

    Removed md5sums.txt file from repo.  We don't need it because we'll be modifying the one copied out of the Porteus Linux .iso.
    Now the remastering shell script actually has executable code in it.
    It might even work.
    
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- remaster_iso/remaster_iso.sh
+++ remaster_iso/remaster_iso.sh
@@ -4,9 +4,78 @@
 #	turns it into a Byzantium Linux .iso image by unpacking, modifying, and
 #	rebuilding it.
 
+# NOTE: This script is intended for developers only.  If you don't plan on
+#	hacking your .iso image you don't need this utility!
+
 # Project Byzantium: http://project-byzantium.org/
 
 # By: The Doctor <drwho at virtadpt dot net>
-
 # License: GPLv3
+
+# Command line arguments:
+#    $1: Location of Porteus Linux .iso image.
+#    $2: Location Byzantium Linux .iso image will be created in.
+#    $3: Path to 000-byzantium.xzm module.
+
+# Count the command line args.  If there aren't any, terminate.
+if [ $# -lt 3 ]; then
+    echo "ERROR: Insufficient command line arguments given to $0."
+    echo "USAGE: $0 /path/to/porteus.iso /path/to/create/byzantium.iso /path/to/000-byzantium.xzm"
+    exit 1
+    fi
+
+# Make sure this script is being run as root.  If it's not, ABEND.
+if [ `id -u` -gt 0 ]; then
+    echo "ERROR: You must be root to run this script."
+    exit 1
+    fi
+
+# This script exists in the directory Byzantium/remaster_iso/.  Thus, we know
+# where we are already, and we know where our towel is as well.  Or in this
+# case, our custom files.
+
+# Store the current working directory in a variable so we can return to it
+# later.
+HOMEDIR=`pwd`
+
+# Mount the Porteus disk image to an unused loop device.
+LOOP=`losetup -f`
+losetup $LOOP $1
+PORTEUS_MOUNTPOINT=`mktemp -d`
+mount $LOOP $PORTEUS_MOUNTPOINT
+
+# Unpack the Porteus .iso image.
+PORTEUS_DIR=`mktemp -d`
+cp -rv $PORTEUS_MOUNTPOINT/* $PORTEUS_DIR
+
+# Copy the Byzantium Linux files into the unpacked .iso image.
+cp byzantium.jpg $PORTEUS_DIR/boot/
+cp porteus.cfg $PORTEUS_DIR/boot/
+cp $3 $PORTEUS_DIR/porteus/modules
+
+# Add the MD5 hash of 000-byzantium.xzm to md5sums.txt.  The way the line is
+# generated is a little awkward because the path the module has to be specified
+# in a particular way.
+HASH=`md5sum $3 | awk '{print $1}'`
+MD5SUM_LINE=`echo $HASH ./modules/000-byzantium.xzm`
+echo $MD5SUM_LINE >> $PORTEUS_DIR/porteus/md5sums.txt
+
+# Make the Byzantium Linux .iso image.
+cd $PORTEUS_DIR/porteus/
+./make_iso.sh $2
+
+# Test to see if the .iso image was created successfully.
+if [ -f "$2" ]; then
+    echo "Byzantium Linux .iso image successfully created at $2."
+else:
+    echo "FAILURE: Byzantium Linux .iso image NOT created!"
+fi
+
+# Clean up after ourselves.
+cd $HOMEDIR
+umount $PORTEUS_MOUNTPOINT
+losetup -d $LOOP
+rm -rf $PORTEUS_MOUNTPOINT $PORTEUS_DIR
+
+exit 0
 
