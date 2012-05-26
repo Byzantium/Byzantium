b5659cd9b466646692970b01fed2a522b88e0f2c
The Doctor <drwho@virtadpt.net>
2012-05-25 22:45:12 -0400

    Updated the services.sqlite database file (and services.sql) to explicitly state their locations in URIs.  This is needed by the services.py CGI-BIN utility to construct URIs to the services.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- control_panel/var/db/controlpanel/services.sql
+++ control_panel/var/db/controlpanel/services.sql
@@ -1,10 +1,10 @@
 BEGIN TRANSACTION;
-CREATE TABLE daemons (initscript TEXT, name TEXT, status TEXT);
-INSERT INTO daemons VALUES('rc.ngircd','IRC server','disabled');
-INSERT INTO daemons VALUES('rc.sshd','SSH','disabled');
-INSERT INTO daemons VALUES('rc.etherpad-lite','Pad','disabled');
-INSERT INTO daemons VALUES('rc.qwebirc','Web Chat','disabled');
-INSERT INTO daemons VALUES('rc.murmur','VoIP Server','disabled');
-CREATE TABLE webapps (name TEXT, status TEXT);
-INSERT INTO webapps VALUES('microblog','disabled');
+CREATE TABLE daemons (port NUMERIC, initscript TEXT, name TEXT, status TEXT);
+INSERT INTO daemons VALUES(6667,'rc.ngircd','IRC server','disabled');
+INSERT INTO daemons VALUES(22,'rc.sshd','SSH','disabled');
+INSERT INTO daemons VALUES(9001,'rc.etherpad-lite','Pad','disabled');
+INSERT INTO daemons VALUES(64738,'rc.murmur','VoIP Server','disabled');
+CREATE TABLE webapps (location TEXT, name TEXT, status TEXT);
+INSERT INTO webapps VALUES('80/microblog','microblog','disabled');
+INSERT INTO webapps VALUES(9090,'chat','disabled');
 COMMIT;
