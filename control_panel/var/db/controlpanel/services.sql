d58ee77bca7b2608ce384d3f51ad1681c4b1427c
The Doctor <drwho@virtadpt.net>
2012-06-02 21:26:11 +0000

    Removed Murmur from services.sql[ite] because it's been removed from the distro until we fix the font problems Pango causes.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- control_panel/var/db/controlpanel/services.sql
+++ control_panel/var/db/controlpanel/services.sql
@@ -2,7 +2,6 @@ BEGIN TRANSACTION;
 CREATE TABLE daemons (port NUMERIC, initscript TEXT, name TEXT, status TEXT);
 INSERT INTO daemons VALUES(6667,'rc.ngircd','IRC server','disabled');
 INSERT INTO daemons VALUES(22,'rc.sshd','SSH','disabled');
-INSERT INTO daemons VALUES(64738,'rc.murmur','VoIP Server','disabled');
 INSERT INTO daemons VALUES(9090,'rc.qwebirc','chat','disabled');
 INSERT INTO daemons VALUES(9001,'rc.etherpad-lite','pad','disabled');
 CREATE TABLE webapps (name TEXT, status TEXT);
