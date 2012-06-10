9684fbe020cac0ab7ef814ed573227f5c46d5496
haxwithaxe <me@haxwithaxe.net>
2012-06-09 21:06:56 -0400

    avahi related *tools* tested an working. need to integrate into respective systems still. ADDED pybonjour module dependancy (NEED TO RESOLVE STILL)

---
+++ control_panel/avahiclient.py
@@ -0,0 +1,72 @@
+import select
+import json
+import sys
+import os
+import pybonjour
+from _utils import *
+
+conf = config()
+
+SERVICE_TYPE = '__byz__._tcp'
+
+timeout  = 5
+resolved = []
+
+def update_services_cache(service,action = 'add'):
+	print(service)
+	print('updating cache')
+	if os.path.exists(conf.services_cache):
+		services = json.loads(file2str(conf.services_cache))
+	else:
+		services = {}
+
+	if action.lower() == 'add':
+		services.update(service)
+		debug('Service added')
+	elif action.lower() == 'del' and service in services:
+		del services[service]
+		debug('Service removed')
+	str2file(json.dumps(services),conf.services_cache)
+
+def resolve_callback(sdRef, flags, interfaceIndex, errorCode, fullname, hosttarget, port, txtRecord):
+	if errorCode == pybonjour.kDNSServiceErr_NoError:
+		print('adding')
+		update_services_cache({fullname:{'host':hosttarget,'port':port,'text':txtRecord}})
+		resolved.append(True)
+
+def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName, regtype, replyDomain):
+	print(serviceName)
+	if errorCode != pybonjour.kDNSServiceErr_NoError:
+		print
+		return
+
+	if not (flags & pybonjour.kDNSServiceFlagsAdd):
+		update_services_cache(serviceName+'.'+regtype+replyDomain,'del')
+		return
+
+	resolve_sdRef = pybonjour.DNSServiceResolve(0, interfaceIndex, serviceName, regtype, replyDomain, resolve_callback)
+
+	try:
+		while not resolved:
+			ready = select.select([resolve_sdRef], [], [], timeout)
+			if resolve_sdRef not in ready[0]:
+				debug('Resolve timed out',1)
+				break
+			pybonjour.DNSServiceProcessResult(resolve_sdRef)
+		else:
+			resolved.pop()
+	finally:
+		resolve_sdRef.close()
+
+if __name__ == '__main__':
+	browse_sdRef = pybonjour.DNSServiceBrowse(regtype = SERVICE_TYPE, callBack = browse_callback)
+	try:
+		try:
+			while True:
+				ready = select.select([browse_sdRef], [], [])
+				if browse_sdRef in ready[0]:
+					pybonjour.DNSServiceProcessResult(browse_sdRef)
+		except KeyboardInterrupt:
+			pass
+	finally:
+		browse_sdRef.close()
---
+++ control_panel/avahiutil.py
@@ -0,0 +1,80 @@
+import os.path
+from _utils import *
+
+services_store_dir = config().services_store_dir
+services_live_dir = config().services_live_dir
+
+
+def _mksname(name):
+	return name.lower().replace(' ','_')
+
+
+def add(name,port,host = '',domain = '',stype = '_http._tcp',subtype = None,protocol = None,text = []):
+	''' Adds service file to storage dir
+		@param	name	service name
+		@param	port	integer port number. (udp: 0-65535, tcp: 1-65535)
+		@param	host	hostname. multicast or unicast DNS findable host name.
+		@param	domain	FQDN eg: myname.local or google.com
+		@param	stype	see http://www.dns-sd.org/ServiceTypes.html (default: '_http._ftp')
+		@param	subtype	service subtype
+		@param	protocol	[any|ipv4|ipv6] (default: any; as per spec in "man avahi.service")
+		@param	text	list/tuple of <text-record/> values
+		USAGE: add('service name',9007) all other parameters are optional
+	'''
+	## notes for haxwithaxe
+	# See http://www.dns-sd.org/ServiceTypes.html
+	# name: <name>1
+	# stype: <service>+
+	# 	protocol: <service ?protocol="ipv4|ipv6|any">
+	# 	domain: <domain-name>?
+	# 	host: <host-name>? (explicit FQDN eg: me.local)
+	#  subtype: <subtype>?
+	#  text: <txt-record>*
+	#  port: <port>? (uint)
+
+	service_tmpl = file2str(config().service_template)
+	service_path = os.path.join(services_store_dir, _mksname(name)+'.service')
+	stext = ''
+	for i in text:
+		stext += '<text-record>'+i.strip()+'</text-record>'
+	service_conf = service_tmpl % {'name':name,'port':port,'host':host,'domain':domain,'stype':stype,'subtype':subtype or '','protocol':protocol or 'any','text':stext}
+	str2file(service_conf, service_path)
+	# activate here?
+
+def activate(name):
+	service_file = os.path.join(services_store_dir,_mksname(name)+'.service')
+	service_link = os.path.join(services_live_dir,_mksname(name)+'.service')
+	if service_file != service_link:
+		if os.path.exists(service_file):
+			if os.path.exists(os.path.split(service_link)[0]):
+				try:
+					os.symlink(service_file,service_link)
+				except Exception as sym_e:
+					return {'code':False,'message':repr(sym_e)}
+			else:
+				return {'code':False,'message':'Directory not found: "%s"' % os.path.split(service_link)[0]}
+		else:
+				return {'code':False,'message':'Directory not found: "%s"' % service_file}
+	reload_avahi_daemon()
+	return {'code':True,'message':'Activated'}
+
+def deactivate(name):
+	service_file = os.path.join(services_store_dir,_mksname(name)+'.service')
+	service_link = os.path.join(services_live_dir,_mksname(name)+'.service')
+	if service_file != service_link:
+		if os.path.exists(service_link):
+			try:
+				os.remove(service_link)
+			except Exception as rm_sym_e:
+				return {'code':False,'message':repr(rm_sym_e)}
+		else:
+				return {'code':False,'message':'Directory not found: "%s"' % service_link}
+		reload_avahi_daemon()
+		return {'code':True,'message':'Deactivated'}
+	else:
+		return {'code':False,'message':'Service file is the same as service link (file:"%s", link:"%s")' % (service_file,service_link)}
+
+def reload_avahi_daemon():
+	import subprocess
+	cmd = ['/usr/sbin/avahi-daemon','--reload']
+	subprocess.call(cmd)
