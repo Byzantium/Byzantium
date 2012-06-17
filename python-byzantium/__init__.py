93ce004122a47a370789b3584d640252c12d4c80
haxwithaxe <me@haxwithaxe.net>
2012-06-17 09:46:16 -0400

    working on an installable module for all the byzantium stuff

---
+++ python-byzantium/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ["service_directory","util","config"]
+__author__ = 'The Doctor, Ben the Pyrate, haxwithaxe' # add your name here if you contribute python code
+__copyright__ = 'Copyright 2012, Byzantium Project'
+__credits__ = '<insert names of people on planet here>'
+__license__ = 'GPL'
+__version__ = '0.1a'
+__maintainer__ = ''
+__email__ = ''
+__status__ = 'Alpha'
+
---
+++ python-byzantium/config/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['global','service_directory','control_panel']
