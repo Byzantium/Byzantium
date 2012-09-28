02b48aa24012263ef684b027f92e04c5fb7f08f4
haxwithaxe <me@haxwithaxe.net>
2012-09-28 19:13:18 -0400

    added mvc model class prototype outline

---
+++ mvc/model/model.py
@@ -0,0 +1,30 @@
+
+class Model(object):
+	'''Model Class'''
+	def __init__(self):
+		pass
+
+	def get(self, **kwargs):
+		'''get
+			retrive one or more
+			'''
+		item = None
+		return item
+
+	def set(self, **kwargs):
+		'''set
+			Create/Update
+			'''
+		pass
+
+	def del(self, **kwargs):
+		'''del
+			remove/delete entry
+			'''
+			pass
+
+	def save(self, file_name=None):
+		'''save
+			save to "disk"
+			'''
+			pass
