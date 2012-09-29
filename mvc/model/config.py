08c46d67b5e7cd9b3568109c710bb011d6a2522e
haxwithaxe <me@haxwithaxe.net>
2012-09-29 18:05:19 -0400

    added doc comments to methods in config.py

--- mvc/model/config.py
+++ mvc/model/config.py
@@ -23,18 +23,21 @@ class Config(model.Model):
     def __init__(self, source):
         self._source = source
         self._config = {}
-        self.__mtime = time.time()
+        self.__mtime = time.time()  # time last modified
 
     def _mod(self):
+        '''set time last modified'''
         self.__mtime = time.time()
 
     def _get_key(self, key):
+        '''get a value for a given key'''
         if key in self._config:
             return self._config[key]
         else:
             return None
 
     def get(self, *args, **kwargs):
+        '''get a value or values for a given key or keys, conditionally on what is specified in kwargs'''
         return_dict = {}
         if len(kwargs) == 1:
             return self._get_key(kwargs['key'])
@@ -60,10 +63,12 @@ class Config(model.Model):
         return return_dict
 
     def _set_key_value(self, key, value):
+        '''set value of a given key to specified value'''
         self._config[key] = value
         return True
 
     def set(self, **kwargs):
+        '''set value of key to value of coresponding keyword in kwargs'''
         return_dict = {}
         if len(kwargs) == 1:
             return self._set_key_value(kwargs.keys()[0], kwargs.values()[0])
@@ -74,6 +79,7 @@ class Config(model.Model):
         return return_dict
 
     def remove(self, *args):
+        '''remove key for given key or keys in args'''
         for key in args:
             if key in self._config:
                 del self._config[key]
@@ -81,6 +87,7 @@ class Config(model.Model):
         return True
 
     def save(self):
+        '''save configuration to file (in this case)'''
         sink_file = open(self._source, 'w')
         json.dump(self._config, sink_file)
         sink_file.close()
@@ -88,17 +95,20 @@ class Config(model.Model):
         return True
 
     def _load(self):
+        '''load configuration from file (in this case) and return it as a string'''
         source_file = open(self._source, 'r')
         config = json.load(source_file)
         source_file.close()
         return config
 
     def load(self):
+        '''load configuration from file (in this case)'''
         self._config = self._load()
         self._mod()
         return True
 
     def sync(self):
+        '''syncronize config file and running config'''
         source = self._load()
         mtime = os.stat(self._source).st_mtime
         if mtime > self.__mtime:
