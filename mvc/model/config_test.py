047908ec56d6130ae329bf92f4b9cef2a32561e6
haxwithaxe <me@haxwithaxe.net>
2012-09-29 17:46:05 -0400

    made test into pyunit test

--- mvc/model/config_test.py
+++ mvc/model/config_test.py
@@ -3,98 +3,100 @@
 
 import copy
 import config
+import json
+import os
+import unittest
 
-'''
-    test(): A method that implements unit tests run on the Model() metaclass.
-'''
-def test():
-    # Can the configuration module be successfully imported and instantiated?
-    print('# Test: import config module')
-    conf = config.Config('example-config.json')
-    print('result: True')
-    print
+config_file_name = 'config-example.json'
+files = [config_file_name]
 
-    # Set a single configuration value.
-    print('# Test: set() with single value')
-    set_one = {'test':'123'}
-    print('info: conf.get() before: %s' % str(conf.get()))
-    set_one_ret_val = conf.set(**set_one)
+class BaseConfigTestCase(unittest.TestCase):
+	def setUp(self):
+		self.files = []
+		self.file_name = config_file_name
+		self.files.append(self.file_name)
+		self.conf = config.Config(self.file_name)
+		self.single_dict_key = 'test'
+		self.single_dict_val = '123'
+		self.fake_config_frag = {'fake': True}
+		self.single_dict = {self.single_dict_key: self.single_dict_val}
+		self.multi_dict = {'test':'321', 'test0':'123', 'test1':'hello', 'test2':'world'}
+		self.fake_config = copy.deepcopy(self.multi_dict)
+		self.fake_config.update(self.fake_config_frag)
 
-    print('info: conf.get() after: %s' % str(conf.get()))
-    print('set %s, returned: %s' % (str(set_one), str(set_one_ret_val)))
+	def tearDown(self):
+		self.cleanup()
 
-    set_one_key = set_one.keys()[0]
-    set_one_val = set_one.values()[0]
-    print('result: set_one == conf.get(set_one.keys()[0]): %s' % (str(set_one_val == conf.get(set_one_key))))
-    print
+	def cleanup(self):
+		for f in self.files:
+			print(f)
+			if os.path.exists(f):
+				os.remove(f)
 
-    # Get a single configuration value.
-    print('# Test: get() with single value')
-    get_ret_val = conf.get(*(set_one.keys()))
-    print('info: get_ret_val %s' % str(get_ret_val))
-    print('info: get value of %s' % set_one.keys()[0])
-    print('result: value of %s == %s: %s' % (set_one.keys()[0], set_one.values()[0], str(get_ret_val == set_one.values()[0])))
-    print
+	def readRawConfig(self):
+		f = open(self.file_name, 'r')
+		d = json.load(f)
+		return d
 
-    # Save and load values to the test datastore to make sure it's working
-    # properly.
-    print('# Test: save() and load()')
+	def writeFakeConfig(self):
+		f = open(self.file_name, 'w')
+		json.dump(self.fake_config, f)
+		f.close()
 
-    # Make an actual duplicate of the configuration data structure rather than
-    # just another reference to it.
-    before = copy.deepcopy(conf.get())
-    save_ret_val = conf.save()
-    load_ret_val = conf.load()
-    after = conf.get()
-    print('result: save successfull: %s' % str(before == after))
-    print
+class SetSingleValueTestCases(BaseConfigTestCase):
+	def runTest(self):
+		'''set single value'''
+		set_one_ret_val = self.conf.set(**self.single_dict)
+		assert self.single_dict_val == self.conf.get(self.single_dict_key), 'set failed to set single value'
 
-    # Store multiple configuration values in a JSON string.  Dictionary.
-    # Whatever.
-    print('# Test: set() and get() with multiple values')
-    multi_dict = {'test':'321', 'test0':'123', 'test1':'hello', 'test2':'world'}
-    set_multi_ret_val = conf.set(**multi_dict)
-    print('info: set multiple values: %s\nreturn == True: %s' % (str(set_multi_ret_val), str(multi_dict)))
-    print
+class GetSingleValueTestCases(BaseConfigTestCase):
+	def runTest(self):
+		'''get single value'''
+		self.conf.set(**self.single_dict)
+		get_ret_val = self.conf.get(self.single_dict_key)
+		assert get_ret_val == self.single_dict_val, 'get failed to load single value'
 
-    # Retrieve those values as a dictionary.
-    print('# Test: Retrieve multiple values from datastore.')
-    get_multi_ret_val = conf.get(*(multi_dict.keys()))
-    print('info: get multiple values: %s' % str(multi_dict.keys()))
-    print('result: output == input: %s' % str(get_multi_ret_val == multi_dict))
-    print
+class SaveTestCases(BaseConfigTestCase):
+	def runTest(self):
+		'''test saving config'''
+		self.conf.set(**self.single_dict)
+		save_ret_val = self.conf.save()
+		raw_config = self.readRawConfig()
+		assert self.single_dict == raw_config, 'save failed to save the config'
 
-    # Synch with data source with before and after printouts to make sure that
-    # what gets saved is what gets loaded.
-    print('# Test: Is what gets saved also what gets loaded?')
-    before = copy.deepcopy(conf.get())
-    ret_val = conf.sync()
-    after = copy.deepcopy(conf.get())
-    expected_after = copy.deepcopy(before)
-    print('info: before: %s' % str(before))
-    print('info: after: %s' % str(after))
-    print('info: ret_val: %s' % str(ret_val))
-    print('result: expected == after: %s' % str(expected_after == after))
-    print
+class LoadTestCases(BaseConfigTestCase):
+	def runTest(self):
+		'''test loading config'''
+		self.writeFakeConfig()
+		self.conf.load()
+		assert self.fake_config == self.conf.get(), 'load failed to load the config'
 
-    # Test the deletion of configuration values from the datastore.
-    print('# Test: Deletion of configuration values from datastore.')
-    removed = 'test'
-    before = copy.deepcopy(conf.get())
-    ret_val = conf.remove(removed)
-    after = copy.deepcopy(conf.get())
-    del before[removed]
-    print('(before - removed) == after: %s' % str((before == after)))
-    print
+class SetGetMultipleTestCase(BaseConfigTestCase):
+	def runTest(self):
+		set_multi_ret_val = self.conf.set(**self.multi_dict)
+		get_multi_ret_val = self.conf.get(*(self.multi_dict.keys()))
+		assert get_multi_ret_val == self.multi_dict, 'save failed to save the config (or get failed to get it)'
 
-    # Test loading configuration settings from a canonical data source to load
-    # the configuration model object.
-    print('Test: Load configuration settings from datastore and configure model object.')
-    ret_val = conf.load()
-    print('conf.load() returned: %s' % str(ret_val))
-    print
+class SyncTestCase(BaseConfigTestCase):
+	def runTest(self):
+		'''sync with source file with before and after printouts'''
+		self.writeFakeConfig()
+		before = copy.deepcopy(self.conf.get())
+		ret_val = self.conf.sync()
+		after = copy.deepcopy(self.conf.get())
+		expected_after = self.fake_config
+		assert expected_after == after, 'sync failed to sync the config file and the config'
 
-# Run the tests.
+class RemoveTestCase(BaseConfigTestCase):
+	def runTest(self):
+		'''remove value (fake)'''
+		removed = self.single_dict_key
+		self.conf.set(**self.multi_dict)
+		before = copy.deepcopy(self.conf.get())
+		ret_val = self.conf.remove(removed)
+		after = copy.deepcopy(self.conf.get())
+		del before[removed]
+		assert before == after, 'remove failed to remove the specified key'
+
 if __name__ == '__main__':
-    test()
-
+	unittest.main()
