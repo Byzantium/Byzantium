89aeac51f7b880e7e3aeab7e84b04170905bbac7
haxwithaxe <me@haxwithaxe.net>
2012-09-29 18:49:58 -0400

    adding more to comments

--- mvc/model/system.py
+++ mvc/model/system.py
@@ -3,6 +3,7 @@
 
 '''
     system.py - *Example* Abstraction of a hardware system to Byzantium systems.
+    System models must not hold values unless absolutely necessery. Holding on to values that can be read from the system is likely to cause errors and unintended behavior of the system when discrepencies between the OS and Byzantium occur.
 '''
 import model
 
@@ -17,25 +18,26 @@ def _do_randomcrap(crap):
     echo(str(crap))
 
 class System(model.Model):
-    '''Hardware Interface Model'''
+    '''NonByzantium System Interface Model (ie OS or Hardware)'''
     def get(self, *agrs,**kwargs):
-        '''get the value or values of the given item or items within the system
+        '''get read the value or values of the given item or items within the system
             return: value requested or None if not found
             '''
         return _is_ipforward()
 
     def set(self, **kwargs):
-        '''set a given value or values within the system
+        '''set a given value or values or act upon the system
             return Boolean (True if success, False if failed)
             '''
         if 'crap' in kwargs:
             _do_randomcrap(kwargs['crap'])
         return True
 
-    def remove(self, **kwargs):
-        '''remove unset a given value or values
+    def remove(self, *args, **kwargs):
+        '''remove or unset a given value or values
             return Boolean (True if success, False if failed)
             '''
+        _do_randomcrap('crap')
         return True
 
     def save(self, sink=None):
