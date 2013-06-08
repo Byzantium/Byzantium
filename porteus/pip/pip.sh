f56e2f8dabd8d63d3b304be54383156c1c93fa2b
The Doctor <drwho@virtadpt.net>
2013-06-08 18:35:58 +0000

    Initial commit of the SlackBuild for Pip, which we need to compile some Python modules.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ porteus/pip/pip.sh
@@ -0,0 +1,9 @@
+# bash completion for pip
+
+_pip_completion()
+{
+    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   PIP_AUTO_COMPLETE=1 $1 ) )
+}
+complete -o default -F _pip_completion pip
