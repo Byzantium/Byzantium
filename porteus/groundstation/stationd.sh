9d5a7282e2c21e428f81fe1026c43d7ef04e3a7d
The Doctor <drwho@virtadpt.net>
2013-06-20 22:46:35 +0000

    Initial commits of two mini script that start the two aspects of Groundstation, stationd on the back-end and airshipd on the front end.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ porteus/groundstation/stationd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# stationd.sh - Tiny shell script that starts stationd.
+# By: The Doctor
+
+# Make sure we are where we need to be.
+cd /opt/groundstation
+
+# Source the custom Python environment.
+. env/bin/activate
+
+# Start stationd in a loop to ensure that it keeps running.
+cd groundstation
+./stationd 2> /dev/null
+
+# Fin.
+exit 0
