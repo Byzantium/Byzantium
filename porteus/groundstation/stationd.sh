1bb2e14f21fc6f1865bbc2156229576344ebb2f2
The Doctor <drwho@virtadpt.net>
2013-07-11 22:45:25 +0000

    Added the environment variable which tells stationd what netblock to broadcast on.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- porteus/groundstation/stationd.sh
+++ porteus/groundstation/stationd.sh
@@ -3,6 +3,10 @@
 # stationd.sh - Tiny shell script that starts stationd.
 # By: The Doctor
 
+# This sets an environment variable for stationd which allows it to broadcast
+# on the mesh because we don't have a 0.0.0.0 route by default.
+export BROADCAST_ADDRESS="192.168.255.255"
+
 # Make sure we are where we need to be.
 cd /opt/groundstation
 
