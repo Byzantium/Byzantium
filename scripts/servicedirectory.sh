9942a8f25a1d4ffa64cb7aaa8546c544853b3692
The Doctor <drwho@virtadpt.net>
2013-07-14 15:46:30 +0000

    Debugged the mini-script that runs the service directory.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- scripts/servicedirectory.sh
+++ scripts/servicedirectory.sh
@@ -7,11 +7,13 @@
 # Make sure we are where we need to be.
 cd /opt/byzantium
 
+pwd
+
 # Source the custom Python environment.
 . env/bin/activate
 
 # Start the service directory daemon.
-python service_directory/services.py 2> /dev/null &
+python service_index/services.py 2>/dev/null &
 
 # Fin.
 exit 0
