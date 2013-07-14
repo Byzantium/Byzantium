d03c049b59fd5d701d94eeeceada16feeb9ec228
The Doctor <drwho@virtadpt.net>
2013-07-14 15:29:03 +0000

    Added a tiny shell script which starts the service directory daemon.
    Updated the etc/issue file before I forget.
    
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ scripts/servicedirectory.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# servicedirectory.sh - Tiny shell script that starts the dynamic service
+#	directory.
+# By: The Doctor [412/724/301/703][ZS]
+
+# Make sure we are where we need to be.
+cd /opt/byzantium
+
+# Source the custom Python environment.
+. env/bin/activate
+
+# Start the service directory daemon.
+python service_directory/services.py 2> /dev/null &
+
+# Fin.
+exit 0
