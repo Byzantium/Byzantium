3d60e82d47449355f821ca55f8f3d16d3822016c
Ben Mendis <dragonwisard@gmail.com>
2014-04-09 20:35:20 -0400

    Don't loop forever. Quit after three tries.

--- byzantium_configd.py
+++ byzantium_configd.py
@@ -89,8 +89,9 @@ if len(wireless):
     # chipsets are pokey (coughAtheroscough) and silently reset themselves if
     # you try to configure them too rapidly, meaning that they drop out of
     # ad-hoc mode.
-    break_flag = False
-    while True:
+    success = False
+    for try_num in range(3):
+        print "Attempting to configure the wireless interface. Try:", try_num
         # Configure the wireless chipset.
         command = ['/sbin/iwconfig', interface, 'mode', 'ad-hoc']
         subprocess.Popen(command)
@@ -145,9 +146,12 @@ if len(wireless):
         # "Victory is mine!"
         #     --Stewie, _Family Guy_
         if Mode and Essid and Bssid and Frequency:
+            success = True
             break
         else:
             print "Failed to setup the interface properly. Retrying..."
+    if not success:
+        sys.exit(1)
 
     # Turn up the interface.
     command = ['/sbin/ifconfig', interface, 'up']
