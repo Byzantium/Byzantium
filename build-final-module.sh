29e31a8d0a12dcfd6524043506640d4891803e24
Ben Mendis <dragonwisard@gmail.com>
2014-04-09 21:27:28 -0400

    This must have changed

--- build-final-module.sh
+++ build-final-module.sh
@@ -189,7 +189,7 @@ cp etherpad-lite/etherpad-lite.service ${FAKE_ROOT}/etc/avahi/inactive
 cp sudo/etc/sudoers ${FAKE_ROOT}/etc
 chown root:root ${FAKE_ROOT}/etc/sudoers
 chmod 0440 ${FAKE_ROOT}/etc/sudoers
-cp avahi/etc/avahi/avahi-daemon.conf ${FAKE_ROOT}/etc/avahi
+#cp avahi/etc/avahi/avahi-daemon.conf ${FAKE_ROOT}/etc/avahi
 cp etc/profile ${FAKE_ROOT}/etc
 
 # Install our custom avahi-dnsconfd.action script.
